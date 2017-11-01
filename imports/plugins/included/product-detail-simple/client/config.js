b06be3a68d3772c556072a38985cc767e8fa92a6
nosisky <nosisky@gmail.com>
2017-11-01 15:35:15 +0100

    bug(hide-firebase-details): Hide firebase configuration details

--- imports/plugins/included/product-detail-simple/client/config.js
+++ imports/plugins/included/product-detail-simple/client/config.js
@@ -1,8 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export default {
-  apiKey: "AIzaSyBKu5-lR3tW1MqTrylEeAgiaYrSk1yz-t4",
+  apiKey: process.env.apiKey,
   authDomain: "matterhorn-rc.firebaseapp.com",
-  databaseURL: "https://matterhorn-rc.firebaseio.com",
-  projectId: "matterhorn-rc",
-  storageBucket: "matterhorn-rc.appspot.com",
-  messagingSenderId: "598428826429"
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId
 };
