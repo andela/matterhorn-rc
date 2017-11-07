35add24344c77e686925e9cbaff7f0bb654f67b1
nosisky <nosisky@gmail.com>
2017-11-07 11:45:41 +0100

    bug(fix-firebase-connection): Fix firebase singleton connection

--- imports/plugins/included/product-detail-simple/client/components/productDetail.js
+++ imports/plugins/included/product-detail-simple/client/components/productDetail.js
@@ -1,6 +1,7 @@
-import React, {Component, PropTypes} from "react";
+import React, { Component, PropTypes } from "react";
 import firebase from "firebase";
-import config from "../config";
+
+import  "../config";
 
 import {
   Button,
@@ -11,13 +12,11 @@ import {
   Toolbar,
   ToolbarGroup
 } from "/imports/plugins/core/ui/client/components/";
-import {AddToCartButton, ProductMetadata, ProductTags, ProductField} from "./";
-import {AlertContainer} from "/imports/plugins/core/ui/client/containers";
-import {PublishContainer} from "/imports/plugins/core/revisions";
+import { AddToCartButton, ProductMetadata, ProductTags, ProductField } from "./";
+import { AlertContainer } from "/imports/plugins/core/ui/client/containers";
+import { PublishContainer } from "/imports/plugins/core/revisions";
 
 import ProductUpload from "./digitalProduct";
-
-firebase.initializeApp(config);
 
 class ProductDetail extends Component {
   constructor(props) {
@@ -27,7 +26,7 @@ class ProductDetail extends Component {
       progress: 0,
       isUploading: false,
       errorMessage: {},
-      isAnalogue: true
+      isnonDigital: true
     };
     this.handleUploadSuccess = this
       .handleUploadSuccess
@@ -38,14 +37,14 @@ class ProductDetail extends Component {
   }
 
   handleUploadSuccess = (filename) => {
-    this.setState({productUrl: filename, progress: 100, isUploading: false});
+    this.setState({ productUrl: filename, progress: 100, isUploading: false });
     firebase
       .storage()
       .ref("products")
       .child(filename)
       .getDownloadURL()
       .then((url) => {
-        this.setState({productUrl: url});
+        this.setState({ productUrl: url });
         this.props.onProductFieldChange(this.product._id, "productUrl", this.state.productUrl);
         this.props.onProductFieldChange(this.product._id, "isDigital", true);
       });
@@ -54,7 +53,7 @@ class ProductDetail extends Component {
   handleChange(event) {
     const value = event.target.value;
 
-    value === "Analogue" ? this.setState({ isAnalogue: true }) : this.setState({ isAnalogue: false });
+    value === "nonDigital" ? this.setState({ isnonDigital: true }) : this.setState({ isnonDigital: false });
   }
 
   get tags() {
@@ -109,12 +108,12 @@ class ProductDetail extends Component {
           </ToolbarGroup>
           <ToolbarGroup>
             <DropDownMenu
-              buttonElement={< Button label = "Switch" />}
+              buttonElement={< Button label="Switch" />}
               onChange={this.props.onViewContextChange}
               value={this.props.viewAs}
             >
-              <MenuItem label="Administrator" value="administrator"/>
-              <MenuItem label="Customer" value="customer"/>
+              <MenuItem label="Administrator" value="administrator" />
+              <MenuItem label="Customer" value="customer" />
             </DropDownMenu>
           </ToolbarGroup>
           <ToolbarGroup lastChild={true}>
@@ -139,80 +138,80 @@ class ProductDetail extends Component {
         position: "relative"
       }}
       >
-      {this.renderToolbar()}
+        {this.renderToolbar()}
 
-      <div
-        className="container-main container-fluid pdp-container"
-        itemScope
-        itemType="http://schema.org/Product"
-      >
-        <AlertContainer placement="productManagement"/>
+        <div
+          className="container-main container-fluid pdp-container"
+          itemScope
+          itemType="http://schema.org/Product"
+        >
+          <AlertContainer placement="productManagement" />
 
-        <header className="pdp header">
-          <ProductField
-            editable={this.editable}
-            fieldName="title"
-            fieldTitle="Title"
-            element={< h1 />}
-            onProductFieldChange={this.props.onProductFieldChange}
-            product={this.product}
-            textFieldProps={{
-              i18nKeyPlaceholder: "productDetailEdit.title",
-              placeholder: "Title",
-              helpText: this.state.errorMessage.title
-            }}
-          />
-
-          <ProductField
-            editable={this.editable}
-            fieldName="pageTitle"
-            fieldTitle="Sub Title"
-            element={< h2 />}
-            onProductFieldChange={this.props.onProductFieldChange}
-            product={this.product}
-            textFieldProps={{
-              i18nKeyPlaceholder: "productDetailEdit.pageTitle",
-              placeholder: "Subtitle"
-            }}
-          />
-        </header>
-
-        <div className="pdp-content">
-          <div className="pdp column left pdp-left-column">
-            {this.props.mediaGalleryComponent}
-            <ProductTags
-              editable={this.props.editable}
+          <header className="pdp header">
+            <ProductField
+              editable={this.editable}
+              fieldName="title"
+              fieldTitle="Title"
+              element={< h1 />}
+              onProductFieldChange={this.props.onProductFieldChange}
               product={this.product}
-              tags={this.tags}
+              textFieldProps={{
+                i18nKeyPlaceholder: "productDetailEdit.title",
+                placeholder: "Title",
+                helpText: this.state.errorMessage.title
+              }}
             />
-            <ProductMetadata editable={this.props.editable} product={this.product}/>
-          </div>
 
-          <div className="pdp column right pdp-right-column">
+            <ProductField
+              editable={this.editable}
+              fieldName="pageTitle"
+              fieldTitle="Sub Title"
+              element={< h2 />}
+              onProductFieldChange={this.props.onProductFieldChange}
+              product={this.product}
+              textFieldProps={{
+                i18nKeyPlaceholder: "productDetailEdit.pageTitle",
+                placeholder: "Subtitle"
+              }}
+            />
+          </header>
 
-            <div className="pricing">
-              <div className="left">
-                <span className="price">
-                  <span id="price">
-                    <Currency amount={this.props.priceRange}/>
-                  </span>
-                </span>
-              </div>
-            </div>
-
-            <div className="vendor">
-              <ProductField
-                editable={this.editable}
-                fieldName="vendor"
-                fieldTitle="Vendor"
-                onProductFieldChange={this.props.onProductFieldChange}
+          <div className="pdp-content">
+            <div className="pdp column left pdp-left-column">
+              {this.props.mediaGalleryComponent}
+              <ProductTags
+                editable={this.props.editable}
                 product={this.product}
-                textFieldProps={{
-                  i18nKeyPlaceholder: "productDetailEdit.vendor",
-                  placeholder: "Vendor"
-                }}
+                tags={this.tags}
               />
+              <ProductMetadata editable={this.props.editable} product={this.product} />
             </div>
+
+            <div className="pdp column right pdp-right-column">
+
+              <div className="pricing">
+                <div className="left">
+                  <span className="price">
+                    <span id="price">
+                      <Currency amount={this.props.priceRange} />
+                    </span>
+                  </span>
+                </div>
+              </div>
+
+              <div className="vendor">
+                <ProductField
+                  editable={this.editable}
+                  fieldName="vendor"
+                  fieldTitle="Vendor"
+                  onProductFieldChange={this.props.onProductFieldChange}
+                  product={this.product}
+                  textFieldProps={{
+                    i18nKeyPlaceholder: "productDetailEdit.vendor",
+                    placeholder: "Vendor"
+                  }}
+                />
+              </div>
 
             <div className="pdp product-info">
               <ProductField
@@ -237,13 +236,13 @@ class ProductDetail extends Component {
                 name="productType"
                 onChange={this.handleChange}
               >
-                <option>Analogue</option>
+                <option>Non Digital</option>
                 <option>Digital</option>
               </select>
               </div>
               }
               
-           { !this.state.isAnalogue &&
+           { !this.state.isnonDigital &&
             <div><p />
             <label>{this.state.progress < 100 && <p>Upload digital product</p>}</label>
             <label>{this.state.progress === 100 && <p>Upload successful</p>}</label>
@@ -253,22 +252,22 @@ class ProductDetail extends Component {
            /></div>}
           </div>
 
-          <div className="options-add-to-cart">
-            {this.props.topVariantComponent}
-          </div>
-          <hr/>
-          <div>
-            <AlertContainer placement="productDetail"/>
-            <AddToCartButton
-              cartQuantity={this.props.cartQuantity}
-              onCartQuantityChange={this.props.onCartQuantityChange}
-              onClick={this.props.onAddToCart}
-            />
+              <div className="options-add-to-cart">
+                {this.props.topVariantComponent}
+              </div>
+              <hr />
+              <div>
+                <AlertContainer placement="productDetail" />
+                <AddToCartButton
+                  cartQuantity={this.props.cartQuantity}
+                  onCartQuantityChange={this.props.onCartQuantityChange}
+                  onClick={this.props.onAddToCart}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    </div>
     );
   }
 }
