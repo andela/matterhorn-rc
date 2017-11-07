d8462d4cc601d7e5e4bdddb78dcd3d0c27061533
Ekundayo Abiona <ekundayoabiona@gmail.com>
2017-11-07 11:03:37 +0100

    feature(embed-social-content): add disqus package for product commenting and sharing on social media sites

--- server/security/policy.js
+++ server/security/policy.js
@@ -29,6 +29,11 @@ BrowserPolicy.content.allowOriginForAll("*.fbcdn.net");
 BrowserPolicy.content.allowOriginForAll("connect.facebook.net");
 BrowserPolicy.content.allowOriginForAll("*.googleusercontent.com");
 
+BrowserPolicy.content.allowOriginForAll("https://disqus.com");
+BrowserPolicy.content.allowOriginForAll("*.disqus.com");
+BrowserPolicy.content.allowOriginForAll("c.disquscdn.com");
+BrowserPolicy.content.allowOriginForAll("*.viglink.com");
+
 BrowserPolicy.content.allowImageOrigin("fbcdn-profile-a.akamaihd.net");
 BrowserPolicy.content.allowImageOrigin("secure.gravatar.com");
 BrowserPolicy.content.allowImageOrigin("i0.wp.com");
